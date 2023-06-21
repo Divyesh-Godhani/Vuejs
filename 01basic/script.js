8a1be0fbfe9dfeea50d6e56aefbdaac6c34c34a7
unknown <godhanidivyesh44@gmail.com>
2023-06-21 09:01:05 +0530

    01basic vue file add

---
+++ 01basic/script.js
@@ -0,0 +1,18 @@
+console.log("divyesh godhani");
+
+const App = {
+    data(){
+        return{
+            name : "divyesh godhani"
+        }
+    }
+}
+Vue.createApp(App).mount('#app');
+
+Vue.createApp({
+    data(){
+        return{
+            name : "i am write code"
+        }
+    }
+}).mount('#onemoreapp');
